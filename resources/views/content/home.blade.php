2952e0f844f062b7e27bad0278fbb252a0db5edb
unknown <webprojectjakarta@gmail.com>
2020-10-31 08:43:31 +0700

    add slideshow home

--- resources/views/content/home.blade.php
+++ resources/views/content/home.blade.php
@@ -118,129 +118,19 @@ function showresult(hitung){
         </div>
 </section>
 
-<!-- silde foto -->
 <section id="questions_page" class="questions-area section-padding">
-    <style>
-    * {box-sizing: border-box;}
-    body {font-family: Verdana, sans-serif;}
-    .mySlides {display: none;}
-    img {vertical-align: middle;}
 
-    /* Slideshow container */
-    .slideshow-container {
-    max-width: 1000px;
-    position: relative;
-    margin: auto;
-    }
-
-/* Caption text */
-.text {
-  color: #f2f2f2;
-  font-size: 15px;
-  padding: 8px 12px;
-  position: absolute;
-  bottom: 8px;
-  width: 100%;
-  text-align: center;
-}
-
-/* Number text (1/3 etc) */
-.numbertext {
-  color: #f2f2f2;
-  font-size: 12px;
-  padding: 8px 12px;
-  position: absolute;
-  top: 0;
-}
-
-/* The dots/bullets/indicators */
-.dot {
-  height: 15px;
-  width: 15px;
-  margin: 0 2px;
-  background-color: #bbb;
-  border-radius: 50%;
-  display: inline-block;
-  transition: background-color 0.6s ease;
-}
-
-.active {
-  background-color: #717171;
-}
-
-/* Fading animation */
-.fade {
-  -webkit-animation-name: fade;
-  -webkit-animation-duration: 1.5s;
-  animation-name: fade;
-  animation-duration: 1.5s;
-}
-
-@-webkit-keyframes fade {
-  from {opacity: .4} 
-  to {opacity: 1}
-}
-
-@keyframes fade {
-  from {opacity: .4} 
-  to {opacity: 1}
-}
-
-/* On smaller screens, decrease text size */
-@media only screen and (max-width: 300px) {
-  .text {font-size: 11px}
-}
-</style>
-</head>
-<body>
-
-        <h2>Automatic Slideshow</h2>
-        <div class="slideshow-container">
-
-        <div class="mySlides fade">
-        <img src="asset/images/header-bg.jpeg" style="width:100%; height:10%;">
-        <div class="text">Caption Text</div>
-        </div>
-
-        <div class="mySlides fade">
-        <img src="asset/images/header-bg.jpeg" style="width:100%">
-        <div class="text">Caption Two</div>
-        </div>
-
-        <div class="mySlides fade">
-        <img src="asset/images/header-bg.jpeg" style="width:100%">
-        <div class="text">Caption Three</div>
-        </div>
-
-        </div>
-        <br>
-
-        <div style="text-align:center">
-        <span class="dot"></span> 
-        <span class="dot"></span> 
-        <span class="dot"></span> 
-        </div>
-
-<script>
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
-</script>
-    </section>
+    <a href="{{url('project')}}">
+                    <div class="page-title text-center">
+                        <h3 class="title">Our Project</h3>
+                    </div>
+            <div class=malasngoding-slider>
+                <div class=isi-slider>
+                    <img src="asset/images/header-bg.jpeg" alt="Gambar 1">
+                    <img src="asset/images/sticky-image.jpg" alt="Gambar 2">
+                    <img src="asset/images/blog-3.jpg" alt="Gambar 3">
+                </div>
+            </div>
+    </a>
+</section>
     <!-- Home-Area-End -->
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,6 +21,7 @@
     <!-- Main-Stylesheets -->
     <link rel="stylesheet" href="{{ asset('asset/css/normalize.css') }}">
     <link rel="stylesheet" href="{{ asset('asset/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('asset/css/slideshow.css') }}">
     <link rel="stylesheet" href="{{ asset('asset/css/responsive.css') }}">
     <script src="{{ asset('asset/js/vendor/modernizr-2.8.3.min.js') }}"></script>
     <script src="https://kit.fontawesome.com/9d10450ac7.js" crossorigin="anonymous"></script>
