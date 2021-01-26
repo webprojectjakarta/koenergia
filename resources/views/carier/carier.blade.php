88ce55bd187417dddafed2fb9c405952306bc159
Rizky <yanandarizky@gmail.com>
2021-01-26 17:18:09 +0700

    fixing careers

--- resources/views/carier/carier.blade.php
+++ resources/views/carier/carier.blade.php
@@ -1,153 +1,3 @@
-<!-- <div class="section-home">
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <div class="carier">
-                    <p>Terima Kasih telah berminat bergabung dengan PT. Kolega Energi Abadi, untuk diperhatikan kepada semua
-                        pelamar untuk mengirimkan detail CV ke alamat email info@koenergia.com dengan subject posisi
-                        yang akan Sdr/i lamar. Pemberitahuan selanjutnya akan dilakukan melalui telepon langsung atau
-                        email.</p>
-                    <p>Regards,</p>
-                    <p>HR. PT. Kolega Energi Abadi</p>
-                </div>
-            </div>
-        </div>
-        <div class="space-30"></div>
-        <div class="row">
-            <div class="col-xs-12">
-                
-            @foreach($datas as $data)
-                <div class="col-md-4 lineVertical">
-                    <h5 class="h5">Needed Urgently</h5>
-                    <h3 class="h3">{{$data->title}}</h3>
-                    <div class="space-20"></div>
-                    <li class="justify">{{$data->requirement1}}</li>
-                    <li class="justify">{{$data->requirement2}}</li>
-                    <li class="justify">{{$data->requirement3}}</li>
-                    <li class="justify">{{$data->requirement4}}</li>
-                    <li class="justify">{{$data->requirement5}}</li>
-                    @if (!$data->requirement9 == NULL || !$data->requirement9 == '')
-                        <li class="justify">{{$data->requirement5}}</li>
-                    @endif
-                    @if (!$data->requirement9 == NULL || !$data->requirement9 == '')
-                        <li class="justify">{{$data->requirement7}}</li>
-                    @endif
-                    @if (!$data->requirement9 == NULL || !$data->requirement9 == '')
-                        <li class="justify">{{$data->requirement8}}</li>
-                    @endif
-                    @if (!$data->requirement9 == NULL || !$data->requirement9 == '')
-                        <li class="justify">{{$data->requirement9}}</li>
-                    @endif
-                    @if (!$data->requirement9 == NULL || !$data->requirement9 == '')
-                        <li class="justify">{{$data->requirement10}}</li>
-                    @endif
-
-                </div>
-            @endforeach
-            </div>
-        </div>
-    </div>
-</div>
- -->
-
- <!-- <section id="questions_page" class="questions-area section-padding-3">
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <div class="page-title text-center">
-                    <h3 class="dark-color">CURRENT OPENINGS</h3>
-                    <div class="space-60"></div>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-xs-12 col-sm-12">
-                <div class="toggole-boxs">
-                    <h3>Apa itu Sistem PLTS Atap?</h3>
-                    <div>
-                        <p style="text-align:justify;">Adalah proses pembangkitan tenaga listrik menggunakan modul
-                            fotovoltaik yang dipasang dan diletakkan diatap, dinding atau bagian lain dari bangunan
-                            milik konsumen PT. PLN (Persero) serta menyalurkan energi listrik melalui sistem sambungan
-                            listrik konsumen PT. PLN (Permen ESDM No. 49, 2018)
-                        </p>
-                    </div>
-                    <h3>Bagaimana cara kerja Sistem PLTS Atap? </h3>
-                    <div>
-                        <p style="text-align:justify;">Modul fotovoltaik (PV Module) akan mengkonversi radiasi matahari
-                            menjadi listrik DC, kemudian output listrik DC tersebut akan masuk ke solar inverter yang
-                            menkonversinya menjadi 220/380 V 50Hz listrik AC, kemudian listrik AC tersebut dapat
-                            digunakan untuk menyalakan peralatan listrik. Jika ada kelebihan energi listrik AC yang
-                            dihasilkan, maka akan diekspor ke jaringan PLN.</p>
-                    </div>
-                    <h3>Bagaimana cara kerja solar inverter? </h3>
-                    <div>
-                        <p style="text-align:justify;">Solar inverter adalah komponen utama atau otak dalam Sistem PLTS
-                            Atap, terutama dalam Sistem On-grid. Saat solar inverter dinyalakan, maka membutuhkan waktu
-                            60 detik untuk mengsinkronkan referensi parameter (tegangan, frekuensi, dll) dari grid PLN
-                            agar inverter dapat berfungsi. Solar Inverter tidak akan berfungsi jika tidak ada listrik DC
-                            dari Module Fotovoltaik dikarenakan bayangan atau terbenamnya matahari dan ketika tidak ada
-                            referensi dari grid PLN atau ketika listrik PLN padam.</p>
-                    </div>
-                    <h3>Apa itu net metering atau kWh ekspor dan impor apa itu pembangkit untuk kepentingan sendiri atau
-                        Self Consumption? </h3>
-                    <div>
-                        <p style="text-align:justify;">kWh ekspor adalah jumlah energi listrik yang disalurkan dari
-                            Sistem PLTS Atap ke sistem jaringan PLN, sedangkan kWh impor adalah jumlah energi listrik
-                            yang diterima dari jaringan PLN yang kemudian keduanya tercata pada kWh meter ekspor impor
-                            (Permen ESDM No. 49, 2018). Sedangkan pembangkit self consumption adalah pembangkit yang
-                            terhubung dengan jaringan PLN akan tetapi tidak ada kegiatan ekspor impor didalamnya, sistem
-                            akan dibatasi berdasarkan beban minimum dalam bangunan.</p>
-                    </div>
-                    <h3>Bagaimana cara perhitungan ekspor impor energi listrik dari Sistem PLTS Atap?</h3>
-                    <div>
-                        <p style="text-align:justify;">- Energi listrik yang diekspor ke jaringan PLN yang tercatat di kWh meter ekspor impor akan
-                            dikalikan 65%.</p>
-                        <p style="text-align:justify;">- Perhitungan energi listrik pelanggan PLTS Atap akan dilakukan setiap bulan berdasarkan
-                            selisih antara kWh impor dengan nilah ekspor</p>
-                        <p style="text-align:justify;">- Apabila nilai kWh ekspor lebih besar dari impor, maka selisih akan diakumulasikan sebagai
-                            pengurangan sebelumnya.</p>
-                        <p style="text-align:justify;">- Selisih yang diperhitungkan akan diakumulasikan paling lama 3 bulan untuk periode tagihan
-                            listrik bulan Januari-Maret, April-Juni, Juli-September atau Oktober-Desember.</p>
-                        <p style="text-align:justify;">- Apabila akumulasi selisih tersisa dipengujung periode, selisih akan dinihilkan dan
-                            perhitungan selisih dimulai pada periode berikutnya.</p>
-                    </div>
-                    <h3>Berapa area atap yang dibutuhkan untuk memasang Sistem PLTS Atap? </h3>
-                    <div>
-                        <p style="text-align:justify;">Untuk 1kWp (1000Wp) sistem, dibutuhkan 6m2 dengan berat 12kg/m2
-                            tergantung ukuran modul fotovoltaik yang digunakan</p>
-                    </div>
-                    <h3>Berapa umur dari Sistem PLTS Atap? </h3>
-                    <div>
-                        <p style="text-align:justify;">Jika Sistem PLTS Atap dipasang dengan baik, sistem dapat
-                            beroperasi sampai dengan 20-25 tahun. Product Warranty dari Modul Fotovoltaik 10 tahun,
-                            Solar Inverter 5 tahun dengan warranty instalasi antara 1-2 tahun.</p>
-                    </div>
-                    <h3>Berapa persenkah penghematan tagihan listrik yang didapat jika memasang Sistem PLTS Atap? </h3>
-                    <div>
-                        <p style="text-align:justify;">Penghematan yang akan didapatkan diantara 30-50 persen dari
-                            tagihan listrik anda setiap bulannya.</p>
-                    </div>
-                    <h3>Siapa sajakah yang dapan melakukan pembangunan dan pemasangan Sistem PLTS Atap? </h3>
-                    <div>
-                        <p style="text-align:justify;">Adapun pemasangan Sistem PLTS Atap wajib dilakukan oleh Badan
-                            Usaha dan lembaga milik pemerintah pusat atau lembaga milik pemerintah daerah yang sudah
-                            terdaftar sebagai Badan Usaha Jasa Penunjang Tenaga Listrik yang telah dipublikasikan pada
-                            laman Direktoran Jenderal Energi Baru, Terbarukan dan Konservasi Energi (Dirjen EBTKE),
-                            Direktorat Jenderal Ketenagalistrikan dan PT PLN (Persero).</p>
-                    </div>
-                    <h3>Bagaimana cara konsultasi untuk mengetahui ukuran yang sesuai untuk bangunan, tahapan atau
-                        persyaratan dan harga dari Sistem PLTS Atap?</h3>
-                    <div>
-                        <p style="text-align:justify;">Untuk konsultasi lebih lanjut bisa menghubungi team kita melalui
-                            messenger atau kirimkan pertanyaan ke massasurya.id@gmail.com. Team kita akan membantu anda
-                            untuk Check ukuran yang anda butuhkan, Select EPC Kontraktor yang sesuai untuk anda dan
-                            kemudian anda bisa Own sistem anda sendiri dengan mudah.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section> -->
 <section id="questions_page" class="questions-area section-padding-3">
     <div class="container">
         <div class="col-xs-12 col-sm-12">
@@ -160,10 +10,10 @@
                     </div>
                 </div>
             <div class="space-100"></div>
-            <div class="row">
-                    <div class="page-title text-center">
-                        <h3 class="dark-color"><b>CURRENT OPENINGS</b></h3>
-                    </div>
+            <div class="row"> 
+                        <div class="page-title text-center">
+                            <h3 class="dark-color"><b>CURRENT OPENINGS</b></h3>
+                        </div>
                     <style>
                     .accordion {
                       background-color: #eee;
@@ -268,16 +118,6 @@
                     </p>
                     </div>
                     @endforeach
-
-                    {{-- <button class="accordion">Section 2</button>
-                    <div class="panel">
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                    </div>
-
-                    <button class="accordion">Section 3</button>
-                    <div class="panel">
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                    </div> --}}
 
                     <script>
                     var acc = document.getElementsByClassName("accordion");
